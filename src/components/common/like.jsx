f2cda2feb0fce616f2a5765c0b81fed266719941
qiuxan <qiuxan@gmail.com>
2021-09-26 21:48:49 +1000

    clean up code

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,20 +1,5 @@
 import React, { } from 'react';
 
-
-
-
-// class Like extends Component {
-
-//     render() {
-//         let classes = this.props.liked ? "fa fa-heart" : "fa fa-heart-o";
-//         return (<i onClick={this.props.onClick} style={{ cursor: 'pointer' }} className={classes} aria-hidden="true"></i>
-//         );
-//     }
-// }
-
-// export default Like;
-
-
 const Like = (props) => {
     let classes = props.liked ? "fa fa-heart" : "fa fa-heart-o";
     return (<i
