c8c423a0552074294d2ae3bc560981adc3ed6ecd
qiuxan <qiuxan@gmail.com>
2021-10-08 15:55:43 +1100

    finished wthe jwt header to local storage

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Joi from "joi-browser";
 import Form from './common/form';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
 
@@ -14,9 +15,34 @@ class LoginForm extends Form {
         password: Joi.string().required().label('Password'),
     }
 
-    doSubmit = () => {
+    doSubmit = async () => {
         //to backend
-        console.log('submitted');
+        // console.log('submitted');
+
+        try {
+
+            const { username, password } = this.state.data;
+            const { data: jwt } = await login(username, password);
+            // console.log(jwt);
+            localStorage.setItem('token', jwt);
+            this.props.history.push('/');
+
+        } catch (ex) {
+
+            if (ex.response && ex.response.status === 400) {
+
+
+                const errors = { ...this.state.errors }
+                errors.username = ex.response.data;
+                // this.setState({ errors });
+                // console.log(errors);
+                this.setState({ errors });
+
+
+
+            }
+        }
+
     }
 
     render() {
--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -19,8 +19,10 @@ class RegisterForm extends Form {
         // console.log('submitted');
 
         try {
-            await userService.register(this.state.data);
-
+            const response = await userService.register(this.state.data);
+            // console.log(response);
+            localStorage.setItem('token', response.headers['x-auth-token']);
+            this.props.history.push('/');
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
                 const errors = { ...this.state.errors }
